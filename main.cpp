4dbbeb390498789f2f0fd92a09c82d3807e0a820
bspy02 <bspy02@hku.hk>
2020-10-18 13:02:28 +0000

    doing 2

--- main.cpp
+++ main.cpp
@@ -1,41 +1,155 @@
 #include <iostream>
+#include <limits>
+#include <iomanip>
 
-const int MAXDIM = 10;
-
-//----------------------DO NOT CHANGE ANYTHING ABOVE THIS LINE------------------
-
-//   Write your function definitions here
 using namespace std;
-void read_matrix(int (&matrix)[MAXDIM][MAXDIM], int &dim)
-{
-    cin >> dim;
-    for (int i = 0; i<dim;i++)
-    {
-        for (int j = 0; j<dim;j++)
-        {
-            cin >> matrix[i][j];
-        }
+
+const int X = 0;
+const int Y = 1;
+
+const int MAXGEOM = 20;
+
+struct AABB {
+  double min[2];  // min[0] = xmin, min[1] = ymin
+  double max[2];  // max[0] = xmax, max[1] = ymax
+
+  // functions
+
+  // Read a point set from user input and calculates AABB
+  void ReadPointSet() {
+    int n;
+    double x, y;
+
+    cin >> n;
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    for (int i = 0; i < n; ++i) {
+      cin >> x >> y;
+      if (x < min[X]) min[X] = x;
+      if (x > max[X]) max[X] = x;
+      if (y < min[Y]) min[Y] = y;
+      if (y > max[Y]) max[Y] = y;
     }
+  }
+
+  // Read a rectangle from user input and calculates AABB
+  void ReadRectangle() {
+    // --- YOUR CODE HERE (Task 1) ---
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    double x, y, w, h;
+    cin >> x >> y >> w >> h;
+    if (x < min[X]) min[X] = x - w/2;
+    if (x > max[X]) max[X] = x + w/2;
+    if (y < min[Y]) min[Y] = y - h/2;
+    if (y > max[Y]) max[Y] = y + h/2;
+  }
+
+  // Read a circle from user input and calculates AABB
+  void ReadCircle() {
+    // --- YOUR CODE HERE (Task 1) ---
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    double x, y, r;
+    cin >> x >> y >> r;
+    if (x < min[X]) min[X] = x - r;
+    if (x > max[X]) max[X] = x + r;
+    if (y < min[Y]) min[Y] = y - r;
+    if (y > max[Y]) max[Y] = y + r;
+  }
+
+};
+
+// return true if two AABBs overlaps
+//        false otherwise
+bool isinside(AABB box, int x, int y) 
+{ 
+    return (x > box.min[0] and x < box.max[0] and y > box.min[1] and y < box.max[1]);
+} 
+bool IsOverlap(AABB p, AABB q) {
+  // --- YOUR CODE HERE (Task 3) ---
+    return isinside(p,q.min[0],q.min[1]) or isinside(p,q.min[0],q.max[1]) or isinside(p,q.max[0],q.min[1])or isinside(p,q.max[0],q.max[1]);
 }
-void display_inner_cw_rotated(int matrix[MAXDIM][MAXDIM], int dim){
-    for (int i = 1; i<dim-1;i++)
-    {
-        for (int j = 1; j<dim-1;j++)
-        {
-            cout << matrix[dim-1-j][i] << " ";
-        }
-        cout <<endl;
-    }
-}
-//----------------------DO NOT CHANGE ANYTHING BELOW THIS LINE------------------
+
 
 int main()
 {
-  int matrix[MAXDIM][MAXDIM]{};
-  int dim = 0;
+  char type;
 
-  read_matrix(matrix, dim);
-  display_inner_cw_rotated(matrix, dim);
+  AABB boxes[MAXGEOM];
+
+  AABB res; // input AAAB and result AABB
+  // initialize bounds of result AABB
+  res.min[X] = res.min[Y] = numeric_limits<double>::max();
+  res.max[X] = res.max[Y] = numeric_limits<double>::lowest();
+
+  // read geometry type and dispatch to the corresponding
+  // AABB read geomety functions to read from user input handle
+  // and compute AABB
+  // store all AABBs in the array "boxes"
+  int numBoxes = 0;
+
+  cin >> type;
+  while (type != '#') {
+    switch (type) {
+      case 'R':
+        boxes[numBoxes++].ReadRectangle();
+        break;
+      case 'C':
+        boxes[numBoxes++].ReadCircle();
+        break;
+      case 'P':
+        boxes[numBoxes++].ReadPointSet();
+        break;
+      default:
+        break;
+    }
+    cin >> type;
+  }
+
+  // display AABBs for input geometries by going through the array "boxes"
+  for (int i = 0; i < numBoxes; i++) {
+    // --- YOUR CODE HERE (Task 2) ---
+    AABB box = boxes[i];
+
+    cout << "AABB";
+    cout << setfill('0') << setw(2);
+    cout << i;
+    cout << " : ";
+    cout << box.min[0] + (box.max[0] - box.min[0])/2 <<" "<< box.min[1] + (box.max[1] - box.min[1])/2 <<" "<< box.max[0] - box.min[0] <<" "<< box.max[1] - box.min[1] <<endl;
+
+  }
+
+  cout << "Isolated AABBs:\n";
+
+  // for each AABB, if it does not overlap with any other AABBs, display its ID
+  // --- YOUR CODE HERE (Task 4) ---
+  for (int i = 0; i < numBoxes; i++)
+  {
+      int sth = 0;
+
+      for (int j = 0; j < numBoxes; j++)
+      {
+          if(i==j)
+          {
+            continue;
+          }
+          else if(sth > 0)
+          {
+              break;
+          }
+          else
+              {sth += IsOverlap(boxes[i], boxes[j]);}
+
+      } 
+      if(sth == 0)
+      {
+          cout << setfill('0') << setw(2);
+          cout << i << endl;
+      }
+  }
+
+
 
   return 0;
 }
